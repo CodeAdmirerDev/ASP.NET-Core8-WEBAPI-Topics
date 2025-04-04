1844696056deb7099acbc22d8a68b89e13a233b1
CodeAdmirer <codeadmirer@gmail.com>
2025-04-05 00:24:48 +0530

    Added the client project for HMAC

---
+++ 4.2 ClientAppForHMAC/HMACHelper.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientAppForHMAC
+{
+    public class HMACHelper
+    {
+
+        public static string GenerateHmacToken(string httpMethodName, string path, string clientId, string secretkey, string requestBody ="")
+        {
+
+            //Generate a nonce (a random value, for example a GUID)
+            var nonce = Guid.NewGuid().ToString();
+
+            //Generate a timestamp (a UNIX timestamp will do)
+            var timestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString();
+
+            //Build the request content by concatenating the HTTP method, the path, the timestamp, the nonce and the request body
+            path = path.StartsWith("/") ? path : "/" + path;
+            Console.WriteLine("path:" + path);  
+            var requestContent = new StringBuilder()
+               .Append(httpMethodName.ToUpper())
+               .Append(path.ToUpper())
+               .Append(nonce)
+               .Append(timestamp);
+
+
+            if (httpMethodName == HttpMethod.Post.Method || httpMethodName == HttpMethod.Put.Method)
+            {
+                requestContent.Append(requestBody);
+            }
+
+            Console.WriteLine("nonce :"+ nonce);
+            Console.WriteLine(requestContent.ToString());
+
+            var secrectbytes = Encoding.UTF8.GetBytes(secretkey);
+            var requestContentBytes = Encoding.UTF8.GetBytes(requestContent.ToString());
+
+            //Compute the hash of the request content using the ComputeHash method of HMACSHA512
+
+            using var hmac = new HMACSHA256(secrectbytes);
+            var computedHash = hmac.ComputeHash(requestContentBytes);
+            var computedToken = Convert.ToBase64String(computedHash);
+
+            return $"{clientId}|{computedToken}|{nonce}|{timestamp}";
+
+
+
+        }
+
+    }
+}
---
+++ 4.2 ClientAppForHMAC/Program.cs
@@ -0,0 +1,88 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+
+namespace ClientAppForHMAC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var clientId = "WebApp";
+            var secretkey = "XYZCompany123";
+            var baseUrl = "https://localhost:7157";// actual Server URL to call the endpoints
+
+            var httpClient = new HttpClient
+            {
+                //Defalut timeout for HttpClient in .NET is 100 seconds, you can override it by setting Timeout property
+                Timeout = TimeSpan.FromMinutes(5),
+            };
+
+            try
+            {
+
+                //Step 1 : Create User object
+
+                var userInfo = new
+                {
+                    Username = "Shiva",
+                    UserRole = "Lead",
+                    Salary = 400000
+                };
+
+                var response = SendRequestAsync(httpClient, HttpMethod.Post, "api/User", clientId, secretkey, baseUrl, userInfo).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = response.Content.ReadAsStringAsync().Result;
+                    Console.WriteLine(responseContent);
+                    Console.WriteLine("User created successfully");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine($"Failed to create User. Status code : {response.StatusCode}");
+                    Console.ReadLine();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+
+        }
+
+        private static async Task<HttpResponseMessage> SendRequestAsync( 
+            HttpClient httpClient, 
+            HttpMethod httpMethod, 
+            string endPoint,
+            string clientId,
+            string secretkey,
+            string baseurl,
+            object data = null)
+        {
+            var requestbody = data == null ? string.Empty : JsonConvert.SerializeObject(data);
+
+            //Generate HMAC token
+            var hmacToken = HMACHelper.GenerateHmacToken(httpMethod.Method, endPoint, clientId, secretkey, requestbody);
+
+            //Create HttpRequestMessage
+
+            var requestMessage = new HttpRequestMessage(httpMethod, $"{baseurl}/{endPoint}")
+            {
+                Content= !string.IsNullOrEmpty(requestbody) ? new StringContent(requestbody, Encoding.UTF8, "application/json") : null
+            };
+
+            //Add Authorization header with HMAC token
+            requestMessage.Headers.Add("Authorization",$"HMAC {hmacToken}");
+
+
+
+            //Send the request
+            return await httpClient.SendAsync(requestMessage);
+
+
+        }
+    }
+}
